0520717e6258f085d3b72a50fb6833ad81bb3017
yujia <yujia@qiniu.com>
2020-12-09 10:03:05 +0800

    Add build script

--- x264/build.sh
+++ x264/build.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`
 ./configure --disable-avs --disable-swscale --disable-lavf --disable-ffms --disable-gpac --disable-lsmash --disable-asm \
 	--disable-opencl --enable-static \
-	--prefix=`pwd`/inst
+	--prefix=`pwd`/inst \
+	&& make install
